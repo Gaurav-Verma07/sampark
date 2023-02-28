58c2dc80023e282017101d07d5ef383c228743fe
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-03-01 04:26:14 +0530

    feat: a basic layout

---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,51 @@
+import {  Header, Container, Group, Burger, Avatar } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+// import { MantineLogo } from '@mantine/ds';
+// import DarkThemeToggle from '../DarkThemeToggle/DarkThemeToggle';
+import { NavLink } from 'react-router-dom';
+import useStyles from './styles';
+
+interface HeaderSimpleProps {
+  links: { link: string; label: string }[];
+}
+
+const MainHeader = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+  //   const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+
+  //   const items = links.map((link) => (
+  //     <a
+  //       key={link.label}
+  //       href={link.link}
+  //       className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+  //       onClick={(event) => {
+  //         event.preventDefault();
+  //         setActive(link.link);
+  //       }}
+  //     >
+  //       {link.label}
+  //     </a>
+  //   ));
+
+  return (
+    <Header height={60} mb={120}>
+      <Container className={classes.header}>
+        {/* <MantineLogo size={28} /> */}
+        <Group spacing={'xl'} className={classes.links}>
+          <NavLink to="profile">
+            <Avatar
+              src="https://images.unsplash.com/photo-1603772655616-711c548d85f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="it's me"
+              radius="xl"
+            />
+          </NavLink>
+          {/* <DarkThemeToggle /> */}
+        </Group>
+
+        <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+      </Container>
+    </Header>
+  );
+};
+export default MainHeader;
---
+++ src/components/ProviderStats/ProviderStats.tsx
@@ -0,0 +1,108 @@
+import { createStyles, ThemeIcon, Progress, Text, Group, Paper } from '@mantine/core';
+import { IconArchive, IconStar } from '@tabler/icons';
+
+const ICON_SIZE = 60;
+
+const useStyles = createStyles((theme) => ({
+  main: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  card: {
+    position: 'relative',
+    overflow: 'visible',
+    width: '30%',
+    padding: theme.spacing.xl,
+    paddingTop: theme.spacing.xl * 1.5 + ICON_SIZE / 3,
+    display: 'flex',
+    flexDirection:"column",
+    justifyContent: 'space-between',
+  },
+
+  icon: {
+    position: 'absolute',
+    top: -ICON_SIZE / 3,
+    left: `calc(50% - ${ICON_SIZE / 2}px)`,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1,
+  },
+}));
+
+const ProviderStats = () => {
+  const { classes } = useStyles();
+
+  return (
+    <Paper className={classes.main} py="md">
+      <Paper radius="md" withBorder className={classes.card} mt={ICON_SIZE / 3}>
+        <ThemeIcon className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE}>
+          <IconStar size={34} stroke={1.5} />
+        </ThemeIcon>
+
+        <Text align="center" weight={700} className={classes.title}>
+          Points Achieved
+        </Text>
+        <Text align="center" weight={400} color="red" py={'sm'} className={classes.title}>
+          Level 1
+        </Text>
+        <Text color="dimmed" align="center" size="sm">
+          32 / 100
+        </Text>
+
+        <Group position="apart" mt="xs">
+          <Text size="sm" color="dimmed">
+            Progress
+          </Text>
+          <Text size="sm" color="dimmed">
+            32%
+          </Text>
+        </Group>
+
+        <Progress value={32} mt={5} />
+
+        <Text size="sm" py="sm" color="dimmed">
+          Get 68 more points to reach Level 2
+        </Text>
+        {/* 
+      <Group position="apart" mt="md">
+        <Badge size="sm">4 days left</Badge>
+      </Group> */}
+      </Paper>
+      <Paper radius="md" withBorder className={classes.card} mt={ICON_SIZE / 3}>
+        <ThemeIcon className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE}>
+          <IconArchive size={34} stroke={1.5} />
+        </ThemeIcon>
+
+        <Text align="center" weight={700} className={classes.title}>
+          Supplies Provided
+        </Text>
+        <Text color="dimmed" align="center" size="sm">
+          20 books and many more
+        </Text>
+
+        {/* <Group position="apart" mt="xs">
+        <Text size="sm" color="dimmed">
+          Progress
+        </Text>
+        <Text size="sm" color="dimmed">
+          32%
+        </Text>
+      </Group> */}
+
+        <Progress value={32} mt={5} />
+
+        <Text size="sm" py="sm" color="dimmed">
+          Get 68 more points to reach Level 2
+        </Text>
+        {/* 
+      <Group position="apart" mt="md">
+        <Badge size="sm">4 days left</Badge>
+      </Group> */}
+      </Paper>
+    </Paper>
+  );
+};
+
+export default ProviderStats;
