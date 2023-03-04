e8721c19cc16139cbfd5a0b45e11b85473adcb50
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-03-04 15:28:50 +0530

    fix: map removed from admin

--- src/Pages/Admin/index.tsx
+++ src/Pages/Admin/index.tsx
@@ -1,4 +1,10 @@
 import { Box, Text } from '@mantine/core';
+// import SimpleMap from '../../components/GoogleMap/GoogleMap';
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+}
 
 const Admin = () => {
   return (
@@ -6,6 +12,7 @@ const Admin = () => {
       <Text color="teal" align="center">
         Welcome to Admin side
       </Text>
+      {/* <SimpleMap location={location} zoomLevel={17} /> */}
     </Box>
   );
 };
--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,14 +1,25 @@
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-const GoogleMap = () => {
-  return (
-    <Map
-      google={this.props.google}
-      zoom={8}
-      style={mapStyles}
-      initialCenter={{ lat: 47.444, lng: -122.176 }}
-    />
-  );
-};
-export default GoogleApiWrapper({
-  apiKey: 'TOKEN HERE',
-})(MapContainer);
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+// import './map.css'
+
+const SimpleMap = ({ location, zoomLevel }: any) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCVrp5nlNyegc98V9ueMQaMqac5YMI769c' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+        >
+          {/* <LocationPin
+            lat={location.lat}
+            lng={location.lng}
+            text={location.address}
+          /> */}
+        </GoogleMapReact>
+      </div>
+    </div>
+  )
+
+  export default SimpleMap;
