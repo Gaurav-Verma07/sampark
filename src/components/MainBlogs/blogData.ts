444178749177a46c575c2afd6709936c6ab77b93
Sakar <sk4sakar@gmail.com>
2023-03-31 15:02:14 +0530

    Values added

--- src/components/MainBlogs/blogData.ts
+++ src/components/MainBlogs/blogData.ts
@@ -1,14 +1,15 @@
+import SamparkLogo from '../../assets/Images/samparklogotransparent.png';
 export const mockdata = [
   {
     title: 'How does Sampark work? See our in-depth values and principles',
     image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+      SamparkLogo,
     date: 'August 18, 2022',
   },
   {
     title: 'Past Event: Cloth Distribution organised at IET Lucknow',
-    image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+    image:"https://drive.google.com/uc?export=download&id=1LvVkueNKCOxWMZhmENlciL98fFnO1Iyl",
+      
     date: 'August 27, 2022',
   },
 ];
