e6184c0c006013e71b2c1a28ec68329ae7e92592
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-05-24 10:47:44 +0530

    API KEY UPDATE

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || '',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || '',
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || '',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || '',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MEASUREMENT_SENDER_ID || '',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || '',
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || '',
+  apiKey:` ${import.meta.env.VITE_FIREBASE_API_KEY} `,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN  }`,
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID  }`  ,
+   databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASE_URL}`|| '',
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCK}` , 
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_SENDER_ID}` , 
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID  
+  }`,
+  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_}` , 
 };
+console.log(firebaseConfig)
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
