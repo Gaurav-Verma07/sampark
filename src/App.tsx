3d8617e9e400d13c401aa68e3817d05e59a20cd3
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-06-27 14:22:35 +0530

    Added Gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Admin from './Pages/Admin';
-import AuthenticationForm from './Pages/Login';
-import RegisterPage from './Pages/Register';
-import Home from './Pages/Home';
-import Provider from './Pages/Provider';
-import Seeker from './Pages/Seeker';
-import Blogs from './components/Blogs/Blogs';
-import AllBlogs from './components/Blogs/AllBlogs';
 import Error from './Pages/Error';
+import Gallery from './Pages/Gallery/Gallery';
+import Home from './Pages/Home';
+import AuthenticationForm from './Pages/Login';
+import Provider from './Pages/Provider';
+import RegisterPage from './Pages/Register';
+import Seeker from './Pages/Seeker';
+import AllBlogs from './components/Blogs/AllBlogs';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery/>}/>
         <Route path="/blogs" element={<AllBlogs />} />
         <Route path="/blogs/:id" element={<Blogs />} />
         <Route path="/login" element={<AuthenticationForm />} />
