b66ea15f1c27c5dfb2f19524b7a7eabfa941e9fb
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-06-28 21:20:08 +0530

    Add preloading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Admin from './Pages/Admin';
@@ -12,21 +13,32 @@ import AllBlogs from './components/Blogs/AllBlogs';
 import Blogs from './components/Blogs/Blogs';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById('loader-wrapper');
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = 'none';
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery/>}/>
-        <Route path="/blogs" element={<AllBlogs />} />
-        <Route path="/blogs/:id" element={<Blogs />} />
-        <Route path="/login" element={<AuthenticationForm />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/provider/*" element={<Provider />} />
-        <Route path="/seeker/*" element={<Seeker />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </div>
+    !loading && (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blogs" element={<AllBlogs />} />
+          <Route path="/blogs/:id" element={<Blogs />} />
+          <Route path="/login" element={<AuthenticationForm />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/provider/*" element={<Provider />} />
+          <Route path="/seeker/*" element={<Seeker />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    )
   );
 }
 
