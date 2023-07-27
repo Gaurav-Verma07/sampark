7a766af4819f4b82031339c8e108893604da3ff9
Suzy <sulagnaghosh52@gmail.com>
2023-07-27 23:00:52 +0530

    feat: add ngo page

--- app/ngos/page.tsx
+++ app/ngos/page.tsx
@@ -1,16 +1,88 @@
 'use client';
-import { Box, Text } from '@mantine/core';
-import { NextPage } from 'next';
+import { Button, Container, createStyles, SimpleGrid } from '@mantine/core';
+import { useRouter } from 'next/navigation';
+import HomeHeader from '../../src/components/HomeHeader/HomeHeader';
+import { data } from '../../src/components/Ngos/ngoContent';
+import './ngo.css';
+import { NgoCard } from '../../src/components/Ngos/NgosCard';
 import React from 'react';
+import { NextPage } from 'next';
 
+const useStyles = createStyles((theme) => ({
+  body: {
+    background: 'white',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+  blog: {
+    marginTop: '2rem',
+    justifyItems: 'center',
+  },
+  back: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: '4%',
+    ...theme.fn.hover({
+      transition: 'transform 150ms ease, box-shadow 150ms ease',
+      transform: 'scale(1.01)',
+      boxShadow: theme.shadows.md,
+    }),
+  },
+}));
 
-const NgosPage: NextPage = () => {
+interface NgosType {
+  id: number;
+  name: string;
+  content: string;
+  author: string;
+  image: string;
+}
+
+const NgosPage: NextPage<NgosType> = () => {
+  const router = useRouter();
+  const { classes } = useStyles();
+
   return (
-    <Box>
-      <Text color="teal" align="center">
-        Welcome to Ngos Page
-      </Text>
-    </Box>
+    <div style={{ margin: '0 3%' }}>
+      <Container className={classes.header}>
+        <HomeHeader />
+      </Container>
+      <div style={{ marginTop: '10%' }}>
+        <Button
+          variant="outline"
+          color="teal"
+          mt={10}
+          className={classes.back}
+          onClick={() => {
+            router.push('/');
+          }}
+        >
+          Go Back
+        </Button>
+        <SimpleGrid
+          className={classes.blog}
+          cols={3}
+          breakpoints={[
+            { maxWidth: 'sm', cols: 1 },
+            { maxWidth: 'md', cols: 2 },
+          ]}
+        >
+          {' '}
+          {data.map((item, index) => (
+            <NgoCard
+              data={item.blogData}
+              image={item.image as string}
+              key={index}
+              index={index}
+            />
+          ))}
+        </SimpleGrid>
+      </div>
+    </div>
   );
 };
 
